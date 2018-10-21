c04bfc65d1dc9534f14d5716f54e12362810596e
Laurent VOULLEMIER <laurent.voullemier@gmail.com>
2018-10-21 08:12:32 +0200

    Add depedencies on setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
     author_email='laurent.voullemier@gmail.com',
     url='https://github.com/l-vo/photos-picker',
     packages=find_packages(exclude=("tests",)),
+    install_requires=['Pillow', 'zope.event', 'dropbox'],
     include_package_data=True,
     zip_safe=False,
     long_description=read('README.md'),
