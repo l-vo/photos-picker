493145a7fb53883df8e89d2254113da177d62497
Laurent VOULLEMIER <laurent.voullemier@gmail.com>
2018-10-24 13:36:28 +0200

    Allow to exclude path from picking

--- photospicker/picker/abstract_picker.py
+++ photospicker/picker/abstract_picker.py
@@ -3,6 +3,7 @@ from photospicker.event.scan_progress_event import ScanProgressEvent
 from zope import event
 import os
 import fnmatch
+import string
 
 
 class AbstractPicker:
@@ -15,7 +16,13 @@ class AbstractPicker:
 
     __metaclass__ = ABCMeta
 
-    def __init__(self, directory_paths, photos_count, patterns=None):
+    def __init__(
+            self,
+            directory_paths,
+            photos_count,
+            patterns=None,
+            excluded_paths=None
+    ):
         """
         Constructor
 
@@ -23,6 +30,7 @@ class AbstractPicker:
         :param int   photos_count:    photos count to pick
         :param list  patterns:        patterns (in lowercase) that files must
                                       match for being scanned
+        :param list  excluded_paths:  directory paths excluded form the scan
         :raise TypeError
         """
         if isinstance(directory_paths, list):
@@ -41,6 +49,13 @@ class AbstractPicker:
 
         self._patterns = patterns
 
+        if excluded_paths is None:
+            excluded_paths = []
+
+        self._excluded_paths = []
+        for excluded_path in excluded_paths:
+            self._excluded_paths.append(os.path.abspath(excluded_path))
+
     @property
     def picked_file_paths(self):
         """Return an array of the picked file paths"""
@@ -50,6 +65,8 @@ class AbstractPicker:
         """Fill in the list of files to scan"""
         for path in self._paths:
             for root, dirnames, filenames in os.walk(path):
+                if self._is_in_excluded_paths(root):
+                    continue
                 for filename in filenames:
                     for pattern in self._patterns:
                         if fnmatch.fnmatch(filename.lower(), pattern):
@@ -57,6 +74,19 @@ class AbstractPicker:
                                 root,
                                 filename
                             ))
+
+    def _is_in_excluded_paths(self, path):
+        """
+        Check if a path is (or is in) an excluded path
+
+        :param string path: path to check
+
+        :return: bool
+        """
+        for excluded_path in self._excluded_paths:
+            if string.find(path, excluded_path) == 0:
+                return True
+        return False
 
     @abstractmethod
     def scan(self):  # pragma: no cover
